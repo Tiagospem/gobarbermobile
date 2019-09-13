ffd8565fd6eda655d523883bc5941c3220d51201
Tiagospem <tiagospem@gmail.com>
2019-09-13 16:01:38 -0300

    added signIn page and bug fix for iOS keyboard

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import {Text} from 'react-native';
 import Background from '~/components/Background';
+import {Image} from 'react-native';
+import logo from '~/assets/logo.png';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignInLink,
+  SignInLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>x</Text>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            autoCorrect={false}
+            placeholder="Sua senha"
+          />
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+        <SignInLink>
+          <SignInLinkText>Criar conta gratuíta</SignInLinkText>
+        </SignInLink>
+      </Container>
     </Background>
   );
 }
